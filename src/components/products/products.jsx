a286dc70d9433f84453c8ed8498b4570b951f066
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-18 13:10:17 -0500

    acción y caso de reducir para el endpoint que recibe todos los productos

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { useSelector, useDispatch, useEffect } from "react";
+import { getProducts } from "../../redux/actions/products";
 
 const products = () => {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(getProducts()));
+  const allProducts = useSelector((store) => store.productsReducer.products);
+  console.log(allProducts);
   return <div></div>;
 };
 
