8b6c1a2b093d816dace838815b7be493c1ea4d77
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-01-18 11:15:04 -0500

    Se crea componente, a falta de data para poder generar funcionalidad, pero con parte de la estructura

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useState } from 'react'
-import {useDipatch} from 'react-redux'
+import React from "react";
+import { useState } from "react";
+import { useDipatch } from "react-redux";
 
-export default function SearchBar(){
-    const dispatch = useDipatch()
-    cons [name, setName] = useState('')
+export default function SearchBar() {
+  const dispatch = useDipatch();
+  cons[(name, setName)] = useState("");
 
-    function handleInputChange(e){
-        e.preventDefault()
-        setName(e.target.value)
-    }
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
 
-    function handleSubmite(e){
-        e.preventDefault()
-        dispatch()
-    }
+  function handleSubmite(e) {
+    e.preventDefault();
+    dispatch();
+  }
 }
