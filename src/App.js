5289cc5c487b7a5ffc87d145ffd2af771dcf967d
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-18 11:52:00 -0500

    reemplaza las hojas de estilos por globalStyles.js para centralizar en un mismo lugar los estilos globales

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { GlobalStyle } from "./globalStyles";
+
 function App() {
-  return <div className="App">Lok App</div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Lok APP</h1>
+      {/* Poner aquí los compenentes que se van a enrutar */}
+    </div>
+  );
 }
 
 export default App;
