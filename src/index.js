afa41de5abcd61ce804d1c3a9adfa056cff46ac4
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-18 09:43:42 -0500

    corrección instalación react

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './store/index.js';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-  <BrowserRouter>    
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </BrowserRouter>
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
