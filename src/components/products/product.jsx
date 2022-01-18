0497ac1740d817546e8813f4d3d600bcbaeb23a5
elecalderon1501 <elecalderon.ec@gmail.com>
2022-01-18 18:34:02 -0300

    Actualizar rama

---
+++ src/components/products/product.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+import React from 'react'
+
+const product = ({id, name, price, img, category}) => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default product
