867868225f72df19366dbe5209c7d0224527b7d0
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-01-18 12:52:40 -0500

    Cambios en componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import {Routes, Route} from 'react-router-dom'
+import Guest from './components/Roles/Guest'
 
 
 
@@ -7,7 +8,7 @@ function App() {
   return 
   <div className="App">Lok App
     <Routes>
-      <Route />
+      <Route exact path="/product" element={<Guest/>}  />
     </Routes>
   </div>;
 
