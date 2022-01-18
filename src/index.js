70bba8e73d257684a194a9c902d542432e13b079
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-18 11:22:35 -0500

    eliminar app.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
