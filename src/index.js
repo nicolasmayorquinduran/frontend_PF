bf5a8709da5b203d2dc619baabc7e8d317d48088
unknown <msosa89@outlook.com>
2022-01-19 13:00:28 -0300

    Modifico index.js para configurar auth0

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App /> 
+      <Auth0Provider
+        domain="dev-cruhauwh.us.auth0.com"
+        clientId="swPjI3JYX1NuJi3gTpHqFlnjAZxtH5Pf"
+        redirectUri={window.location.origin}>
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </Provider>,
 
