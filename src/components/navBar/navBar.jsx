928d7aa3669098ec887d68a4a7dd254c36087c20
etchepared <etcheparede@gmail.com>
2022-01-19 14:37:42 -0300

    navBarMenu dropdown added to navBar component and login updated

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LoginButton } from "../Login/Login";
+import { NavBarMenu } from "./navBarMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
-
 import "./navbar.css";
 
 const NavBar = () => {
   return (
     <div className="navbarContainer">
-      <Link id="navLink" to="/">
-        <FontAwesomeIcon icon={faBars} />
-      </Link>
+      <NavBarMenu />
       <Link id="navLink" to="/">
         <FontAwesomeIcon icon={faSearch} />
       </Link>
       <div className="vacio"></div>
       <h1>Henry LooK</h1>
       <div className="vacio"></div>
-      <Link id="navLink" to="/">
-        <FontAwesomeIcon icon={faUser} />
-      </Link>
+      <LoginButton />
       <Link id="navLink" to="/">
         <FontAwesomeIcon icon={faCartPlus} />
       </Link>
