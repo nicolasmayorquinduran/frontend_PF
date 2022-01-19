a4c5cbe6be9681bc3bec5c2f90217314a50ba976
oiaraque <oiaraque@gmail.com>
2022-01-19 13:35:14 -0500

    paginado completo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { GlobalStyle } from "./globalStyles";
-import Products from "./components/products/products";
 import ProductsDetail from "./components/ProductDetails/ProductDetails";
+
+import SearchBar from './components/SearchBar/SearchBar.jsx';
+import Products from './components/products/products.jsx';
+
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <h1>Lok APP</h1>
+      <SearchBar />
       <Products />
     </div>
   );
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,24 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../../redux/actions/products";
 import { Container } from "../../globalStyles";
-import SearchBar from "../SearchBar/SearchBar";
+
 import Product from "./product";
+import Paginado from '../Paginado/Paginado.jsx';
 
 const Products = () => {
   const dispatch = useDispatch();
-  useEffect(() => dispatch(getProducts()), [dispatch]);
   const allProducts = useSelector((store) => store.productsReducer.products);
+  useEffect(() => dispatch(getProducts()), [dispatch]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(9);
+  const indexOfLastProduct = currentPage*productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProduct = allProducts.slice(indexOfFirstProduct,indexOfLastProduct);
+  const paginado = (pageNumber)=>{setCurrentPage(pageNumber)};
+
   console.log(allProducts);
 
   return (
     <div>
-      <SearchBar />
       <Container>
-        {allProducts.map((p) => (
-          <Product name={p.name} img={p.img} price={p.price} />
-        ))}
+      {
+        currentProduct?.map(product=>{
+          return(
+            <div>
+              <Product 
+               img={product.img}
+               name={product.name}
+               price={product.price} 
+              />
+            </div>
+          )
+        })
+      }
       </Container>
+      <Paginado 
+        productsPerPage={productsPerPage}
+        allProducts={allProducts.length}
+        paginado={paginado}
+      />
     </div>
   );
 };
