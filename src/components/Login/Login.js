093e0de54fe6aa8856acf5fcae3957c82a01a6bc
unknown <msosa89@outlook.com>
2022-01-19 17:29:06 -0300

    Componentes logout y profile

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
-    return <button onClick={() => loginWithRedirect()}>Login</button>
+    return <FontAwesomeIcon onClick={() => loginWithRedirect()} icon={faUser} />;
 }
