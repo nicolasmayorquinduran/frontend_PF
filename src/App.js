31b5b9188a0e4ec1a0d59aa2f2f01fb52b28c95f
etchepared <etcheparede@gmail.com>
2022-01-19 12:37:41 -0300

    NavBar created and added to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import { GlobalStyle } from "./globalStyles";
-
+import NavBar from "./components/navBar/navBar.jsx"
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <NavBar />
       <h1>Lok APP</h1>
       {/* Poner aquí los compenentes que se van a enrutar */}
     </div>
