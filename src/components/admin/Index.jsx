6a5838da63e06a557502ad9b0b91972b691a8495
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-01-21 10:12:37 -0500

    Se generan actions, reducer y componente adminOrders, En el componente queda pendiente el renderizaado de el diseño. En el cual estoy trabajando

--- src/components/admin/Index.jsx
+++ src/components/admin/Index.jsx
@@ -6,12 +6,13 @@ import AdminCategories from "./AdminCategory/AdminCategories";
 import AdminUsers from "./AdminUsers/AdminUsers";
 import AdminrOrders from "./AdminOrders/AdminOrders";
 import AdminProducts from "./AdminProducts/AdminProducts";
-import Products from "../users/products/products";
-import AdminOrders from "./AdminOrders/AdminOrders";
+/* import Products from "../users/products/products";
+import AdminOrders from "./AdminOrders/AdminOrders"; */
 
 /* componente prestacional del administrador con un menú de opciones disponibles (productos, pedidos, categorías y usuarios), debe renderizar determinado componente según la pestaña que se clickee */
 
 function Index() {
+  
   const [local, setLocal] = useState("");
 
   function handleClick(e) {
