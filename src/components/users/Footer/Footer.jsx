b3a4d858b25d3e7d9cac796cf38144fe0b0aac1a
etchepared <etcheparede@gmail.com>
2022-01-22 03:46:26 -0300

    ProductDetails.jsx styled with productdetails.css, component Footer created, styled with footer.css and added to App.js

---
+++ src/components/users/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./footer.css";
+import { FiTwitter } from "react-icons/fi";
+import { AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
+import { FiInstagram } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h2>¡ Síguenos !</h2>
+      <h3>Te mantendremos a la vanguardia urbana.</h3>
+      <hr></hr>
+      <div>
+        <Link to="www.facebook.com">
+          <AiOutlineFacebook className="iconsSM" />
+        </Link>
+        <Link to="www.twitter.com">
+          <FiTwitter className="iconsSM" />
+        </Link>
+        <Link to="www.instagram.com">
+          <FiInstagram className="iconsSM" />
+        </Link>
+        <Link to="www.linkedin.com">
+          <AiOutlineLinkedin className="iconsSM" />
+        </Link>
+      </div>
+      <hr></hr> <hr></hr>
+      <h5>Desarrollado por FraternidadJS</h5>
+    </div>
+  );
+};
+
+export default Footer;
