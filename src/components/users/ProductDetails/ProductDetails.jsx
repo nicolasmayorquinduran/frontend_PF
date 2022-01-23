6308e441a82ef723c8a228a19b49288ab25a5737
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-23 18:37:03 -0500

    modularization of admin products component

--- src/components/users/ProductDetails/ProductDetails.jsx
+++ src/components/users/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { detailsProduct,getProducts } from "../../../redux/actions/products";
+import { detailsProduct, getProducts } from "../../../redux/actions/products";
 import "./productdetails.css";
 import Cart from "../Cart/Cart";
 import { useParams } from "react-router-dom";
@@ -9,24 +9,17 @@ import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ProductDetails() {
-  const {id}=useParams()
+  const { id } = useParams();
   const dispatch = useDispatch();
-  const product = useSelector((store) => store.productsReducer.products.find(e=>e.id == id));
-  useEffect(() =>{
-      dispatch(getProducts())
-      dispatch(detailsProduct())
-    },[dispatch]
-  );
-  console.log("PRODUCTO FILTRADO",product)
-  const handleAddCart = ()=>{
-    dispatch()
-  }
+  useEffect(() => {
+    dispatch(getProducts());
+    dispatch(detailsProduct());
+  }, [dispatch]);
 
   const [changeInfo, setChangeInfo] = useState("Comentarios");
-
-  useEffect(() => dispatch(detailsProduct()), [dispatch]);
-
-  const product = useSelector((store) => store.productsReducer.productsDetails);
+  const product = useSelector((store) =>
+    store.productsReducer.products.find((e) => e.id == id)
+  );
 
   const [bigImage, setBigImage] = useState(0);
 
@@ -59,7 +52,6 @@ export default function ProductDetails() {
               </div>
 
               <div className="smallImg">
-
                 {product.images.map((image, index) => (
                   <img
                     src={image}
@@ -73,9 +65,9 @@ export default function ProductDetails() {
 
             <div className="productDetail">
               <h2> {product.name} </h2>
-              <h3 id="price"> {formatMoney(product.price) } </h3>
-              <input type="number" name="qty"/>
-              <br></br>   
+              <h3 id="price"> {formatMoney(product.price)} </h3>
+              <input type="number" name="qty" />
+              <br></br>
               <div id="categoriesContainer">
                 <h6 id="categories"> Categories: </h6>
                 {product.type}
