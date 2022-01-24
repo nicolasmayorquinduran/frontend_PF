71e99abefcfec369f61d3120b984460ea986086e
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-01-24 09:12:47 -0500

    Se genera componente orderAdmin

--- src/components/admin/AdminOrders/AdminOrders.jsx
+++ src/components/admin/AdminOrders/AdminOrders.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import "./AdminOrders.css";
 
 /* Actions  */
@@ -12,19 +14,86 @@ const AdminOrders = () => {
   useEffect(() => dispatch(orderAdmin()), [dispatch]);
 
   const order = useSelector((state) => state.orderAdmReducer.orders);
+  console.log(order);
 
   return (
     <div className="principal-container">
-      <img src={order.image} alt="Not found" width="500px" height="600px" />
-      <div className="productName">{order.productName}</div>
-      <div>{order.price}</div>
-      <div>{order.status}</div>
-      <div>{order.description}</div>
-      <div>
-        <h5>Pedido realizado por:</h5>
-      </div>
+      {order != undefined || order.length ? (
+        order.map((el) => {
+          return (
+            <div className="container1">
+              <h4>Productos</h4>
+              <br />
+              <ul>
+                <th>
+                  <a>Estado</a>
+                  <br />
+                  <select name="" id="">
+                    <option value="">Pendiente</option>
+                    <option value="">Cancelado</option>
+                    <option value="">Entregado</option>
+                  </select>
+                </th>
+                <th>
+                  <a>Usuario:</a>
+                  <br />
+                  <h6> {el.client.name}</h6>
+                </th>
+                <th>
+                  <a>Id:</a>
+                  <br />
+                  <h6> {el.products.map(el => <li>{el.id}</li>)}</h6>
+                </th>
+                <th>
+                  <a>Producto: </a>
+                  <br />
+                  <ul>
+                    {el.products.map((el) => (
+                      <li>{el.name}</li>
+                    ))}
+                  </ul>
+                </th>
+                <th>
+                  <a>Cantidad: </a>
+                  <br />
+                  <ul>
+                    {el.products.map((el) => (
+                      <li>{el.cantidad}</li>
+                    ))}
+                  </ul>
+                </th>
+                <th>
+                  <a>Total x Producto:</a>
+                  <br />
+                  <ul>
+                    {el.products.map((el) => (
+                      <li>{el.price}</li>
+                    ))}
+                  </ul>
+                </th>
+                <th>
+                  <a>Total Compra:</a>
+                  <br />
+                   <h6>${el.products.map(el => (<li>{el.price}</li>))}</h6> 
+                </th>
+                <th>
+                  <a>Ver Detalle:</a>
+                  <br />
+                  <FontAwesomeIcon icon={faEye} />
+                </th>
+              </ul>
+              <br />
+              <br />
+            </div>
+          );
+        })
+      ) : (
+        <h2>Cargando productos</h2>
+      )}
     </div>
   );
 };
 
 export default AdminOrders;
+
+
