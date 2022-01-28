ee3bab6de07f39346d070e0bc15cad573dd2c5e6
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-27 21:00:08 -0500

    add conditional render and styles to admin Promo tab

--- src/components/admin/AdminPromos/AdminPromos.jsx
+++ src/components/admin/AdminPromos/AdminPromos.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPromos } from "../../../redux/actions/promos";
 import { useDispatch, useSelector } from "react-redux";
 import CreatePromo from "./CreatePromo";
@@ -7,17 +7,52 @@ import { Container, Children } from "../../../globalStyles";
 import "./style.css";
 
 const AdminPromos = () => {
+  const [product, setProduct] = useState({
+    title: "",
+    img: "",
+    resume: "",
+  });
   const dispatch = useDispatch();
-  useEffect(() => dispatch(getPromos()), [dispatch]);
+  useEffect(() => dispatch(getPromos()), [dispatch, product]);
   const allPromos = useSelector((store) => store.promoReducer.promos);
   return (
     <div>
-      <CreatePromo />
-      <EditPromo />
-      <div className="create"></div>
+      {product.title.length ? (
+        <EditPromo
+          title={product.title}
+          img={product.img}
+          resume={product.resume}
+        />
+      ) : (
+        <CreatePromo />
+      )}
+
       <Container className="edit">
+        <Children
+          className="create"
+          onClick={() =>
+            setProduct({
+              title: "",
+              img: "",
+              resume: "",
+            })
+          }
+        >
+          <p></p>
+          <h2>+</h2>
+          <p>Crear nueva Promo</p>
+        </Children>
         {allPromos.map((p) => (
-          <Children className="miniature">
+          <Children
+            className="miniature"
+            onClick={() =>
+              setProduct({
+                title: p.title,
+                img: p.img,
+                resume: p.resume,
+              })
+            }
+          >
             <div className="parts">
               <img src={p.img} alt={p.resume} />
             </div>
