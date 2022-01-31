1bc96f37f244e627916408dff6cf29d203e94234
unknown <msosa89@outlook.com>
2022-01-30 21:04:41 -0300

    delete user

--- src/components/users/Login/Settings.jsx
+++ src/components/users/Login/Settings.jsx
@@ -5,9 +5,11 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { putUser } from "../../../redux/actions/users";
 import { useDispatch } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Settings = () => {
     const dispatch = useDispatch();
+    const { logout } = useAuth0();
     const actualUser = useSelector((store) => store.actualUser);
     const [userEdited, setUserEdited ]= useState({
         email: "",
@@ -36,11 +38,22 @@ export const Settings = () => {
     function handleSubmit(e){
         if(userEdited.name !== ""){
             e.preventDefault();
-            dispatch(putUser(userEdited));
             alert("Datos guardados");
+            dispatch(putUser(userEdited));
         } else {
             e.preventDefault();
             alert("Complete el nombre");
+        }
+    }
+
+
+    function handleDelete(e){
+        e.preventDefault();
+        let choose = window.confirm("Está seguro que desea eliminar la cuenta?")
+        if (choose){
+            
+            logout({ returnTo: window.location.origin })
+            dispatch(putUser({email: actualUser.email, del: true}));
         }
     }
 
@@ -48,7 +61,6 @@ export const Settings = () => {
         <div>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <h1>Tus datos</h1>
-                <h3>Imagen</h3>
                 <img src={actualUser.picture} alt="not found" />
                 <h3>Email</h3>
                 <input type="text" value={actualUser.email} disabled />
@@ -62,6 +74,7 @@ export const Settings = () => {
                 <input type="text" defaultValue={actualUser.state} name="state" onChange={e => handleChange(e)} />
                 <button type='submit' >Guardar</button>
             </form>
+                <button onClick={(e) => handleDelete(e)} >Eliminar cuenta</button>
         </div>
     );
 }
