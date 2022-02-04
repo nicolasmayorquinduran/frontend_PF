9e8dfdf631b6f4e48059ffbc1a965677278d3d9b
etchepared <88214684+etchepared@users.noreply.github.com>
2022-02-04 18:29:42 -0300

    added navBarCart.jsx and changes on navBar to import navBarCart

--- src/components/users/navBar/navBar.jsx
+++ src/components/users/navBar/navBar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { NavBarUser } from "./navBarUser";
+import { NavBarCart } from "./navBarCart";
 import { NavBarMenu } from "./navBarMenu";
+import { NavBarUser } from "./navBarUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import logoLok from "./logo_LOK.png";
 import "./navbar.css";
+import { getUserCart } from "../../../redux/actions/products";
+import { useDispatch } from "react-redux";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const email = window.localStorage.getItem("userEmail");
+
+  useEffect(() => {
+    dispatch(getUserCart(email));
+  }, [dispatch]);
+
   return (
     <div className="navbarContainer">
       <NavBarMenu />
@@ -19,9 +29,8 @@ const NavBar = () => {
       </div>
 
       <NavBarUser />
-      <Link id="navLink" to="/cart">
-        <FontAwesomeIcon icon={faCartPlus} />
-      </Link>
+
+      <NavBarCart />
     </div>
   );
 };
