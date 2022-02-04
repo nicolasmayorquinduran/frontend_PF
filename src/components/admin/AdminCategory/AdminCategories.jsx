963ac60f334b31d420233afb54a7d0428a33d788
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-02-04 18:50:09 -0500

    Se genera componente edit

--- src/components/admin/AdminCategory/AdminCategories.jsx
+++ src/components/admin/AdminCategory/AdminCategories.jsx
@@ -1,53 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "../../../redux/actions/categories";
 import "./adminCategories.css";
+
+//COMPONENTS
 import NewCategory from "./NewCategory";
-/* import { Image } from 'cloudinary-react'; */
+import EditCategories from "./EditCategories";
 
 function AdminCat() {
-  const categories = useSelector(
-    (state) =>
-      (state.filterCategories.length && state.filterCategories) ||
-      state.categories
-  );
-  // const [categories, setCategories] = useState(DBcategories); //hay que importarlo si lo usan
-  // console.log(categories);
+  const categories = useSelector((state) => state.categories);
 
   //INSERTAR IMAGEN
-  // const [imageSelected, setImageSelected] = useState("");
+  /* const [imageSelected, setImageSelected] = useState(""); */
   // console.log(imageSelected)
+  const [showEditCategories, setShowEditCategories] = useState(false);
 
-  // const [newCategory, setNewCategory] = useState({
-  //   name: "",
-  //   active: true,
-  //   img: imageSelected,
-  // });
+  const [category, setCategory] = useState({
+    CategoriesId:"",
+    name: "",
+    img:""   
+  });
 
+  
   const dispatch = useDispatch();
-  useEffect(() => dispatch(getCategories()), [dispatch]); //agregar imageSelected si lo usas
+  useEffect(() => dispatch(getCategories()), [dispatch]);
+
+  const handleImageClick = (c) => {
+    setCategory(c)
+    setShowEditCategories(true);
+    console.log(c)
+  };
 
   return (
     <>
-      <NewCategory />
-      <div className="categoriesCards">
-        {categories?.map((c) => {
-          return (
-            <div key={c.CategoriesId} className="catCard">
-              <div className="labelAndDelete">
-                <label className="catLabel">{c.name}</label>
+      {showEditCategories ? <EditCategories category={category} setCategory={setCategory} /> : null}
 
-                <button className="deleteBtn" id={c}>
-                  X
-                </button>
+      {!showEditCategories ? <NewCategory /> : null}+
+      <div>
+        <div className="categoriesCards">
+          {categories?.map((c, i) => {
+            return (
+              <div className="catCard" key={i}>
+                <div className="labelAndDelete">
+                  <label className="catLabel">{c.name}</label>
+
+                  <button className="deleteBtn" id={c}>
+                    X
+                  </button>
+                </div>
+                <div
+                  className="imagenCard"
+                  onClick={()=> handleImageClick(c)}
+                  style={{ backgroundImage: `url(${c.img})` }}
+                ></div>
               </div>
-              <div
-                className="imagenCard"
-                style={{ backgroundImage: `url(${c.img})` }}
-              ></div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
