dc849327ddf46a1a29730b2cc2515324c6ca8662
etchepared <88214684+etchepared@users.noreply.github.com>
2022-02-05 18:54:51 -0300

    commented code

--- src/components/users/navBar/navBarCart.jsx
+++ src/components/users/navBar/navBarCart.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUserCart } from "../../../redux/actions/products";
 
 export const NavBarCart = () => {
+  // const dispatch = useDispatch();
+
   const cartItems = useSelector(
     (state) =>
       state.cart.hasOwnProperty("productCart") && state.cart.productCart
   );
+
+  // useEffect(() => dispatch(getUserCart()), [dispatch]);
 
   return (
     <>
