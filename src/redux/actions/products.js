ed38684e835143ace97f3f3b92e8848a9ed5f40f
ElianZK <elianh2015@gmail.com>
2022-02-07 17:31:26 -0300

    pre pull dev, cartToBuy action done

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -126,3 +126,17 @@ export function updateProductAdm(payload) {
     }
   }
 }
+
+export function cartToBuy(CartId,infoBuy,infoUser){
+  return async function(dispatch){
+    try {
+      const buy = await axios.post(`http://localhost:3001/order/${CartId}`,{infoBuy,infoUser})
+    return dispatch({
+      type:TYPES.CART_TO_BUY,
+      payload: buy.data
+    })
+    } catch (error) {
+      
+    }
+  }
+}
