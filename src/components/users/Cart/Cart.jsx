232f65ec36525151676fc8bbee780aa2507316e2
unknown <msosa89@outlook.com>
2022-02-09 13:00:40 -0300

    redirect login

--- src/components/users/Cart/Cart.jsx
+++ src/components/users/Cart/Cart.jsx
@@ -164,7 +164,10 @@ export default function Cart() {
         {User.hasOwnProperty("UsersId") ? (
           <Button onClick={toggle}>GO TO CHECKOUT</Button>
         ) : (
-          <Button onClick={() => loginWithRedirect()}>
+          <Button onClick={() => {
+            window.localStorage.setItem("url", window.location.pathname)
+            loginWithRedirect()
+        }}>
             Finalizar tu compra!
           </Button>
         )}
