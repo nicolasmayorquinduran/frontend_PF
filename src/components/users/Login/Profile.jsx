9b4704d4c2d2d5fa0649dad41747114d403b285a
etchepared <88214684+etchepared@users.noreply.github.com>
2022-05-24 10:17:38 -0300

    esto es un cambio de prueba

--- src/components/users/Login/Profile.jsx
+++ src/components/users/Login/Profile.jsx
@@ -6,7 +6,7 @@ import { addToCart } from "../../../redux/actions/products";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { useNavigate } from "react-router"; 
+import { useNavigate } from "react-router";
 import "./profile.css";
 
 let noRepeat = false;
@@ -28,10 +28,13 @@ export const Profile = () => {
       noRepeat = true;
       await dispatch(getActualUser(user.email));
       window.localStorage.setItem("userEmail", user.email);
-      if (window.localStorage.url && window.localStorage.url !== window.location.pathname) {
-        navigate(window.localStorage.url)
+      if (
+        window.localStorage.url &&
+        window.localStorage.url !== window.location.pathname
+      ) {
+        navigate(window.localStorage.url);
       }
-      window.localStorage.removeItem("url")
+      window.localStorage.removeItem("url");
     }
   }, [user, dispatch]);
   const actualUser = useSelector((store) => store.actualUser);
